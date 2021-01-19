18c9ff3c5cfafa21811b25f1e99db8d8e7973f9b
psn2706 <sergei.petrov.com@mail.ru>
2021-01-20 00:55:26 +0300

    try except

--- serv.py
+++ serv.py
@@ -6,14 +6,17 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
     # noinspection PyAttributeOutsideInit
     def handle(self):
-        self.data = self.request.recv(1024).strip()
+        self.data = self.request.recv(2 ** 20).strip()
         print("{} wrote:".format(self.client_address[0]))
         print(str(self.data, "utf-8"))
-        self.request.sendall(self.data.upper()[1:])
+        self.request.sendall(self.data.upper())
 
 
 if __name__ == "__main__":
     HOST, PORT = socket.gethostname(), 8000
 
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print()
