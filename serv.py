bdd139eeca61d03906f6b8af65fed44ccfec1c56
psn2706 <sergei.petrov.com@mail.ru>
2021-02-07 19:32:49 +0300

    with value

--- serv.py
+++ serv.py
@@ -1,16 +1,17 @@
 import socketserver
 import socket
 
-
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     # noinspection PyAttributeOutsideInit
-    def handle(self):
+    def handle(self, k=0):
         self.data = self.request.recv(2 ** 20).strip()
         s = '\nПривет, мой друг!'
         print("{} wrote:".format(self.client_address[0]))
         print(str(self.data, "utf-8"))
         self.data = self.data+s.encode("utf-8")
+        k = k + 1
+        print(k)
         self.request.sendall(self.data.upper())
 
 
@@ -19,6 +20,7 @@ if __name__ == "__main__":
 
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
     try:
+        print(f'Connected to {HOST}:{PORT}')
         print('Quit the server with CTRL-BREAK.')
         server.serve_forever()
     except KeyboardInterrupt:
