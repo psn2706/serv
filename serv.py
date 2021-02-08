eef4678047e28df04afafec74a3352de8b9cd16c
psn2706 <sergei.petrov.com@mail.ru>
2021-02-08 15:17:52 +0300

    With threads

--- serv.py
+++ serv.py
@@ -1,27 +1,40 @@
 import socketserver
 import socket
+import threading
+
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
 
     # noinspection PyAttributeOutsideInit
-    def handle(self, k=0):
+    def handle(self):
         self.data = self.request.recv(2 ** 20).strip()
         s = '\nПривет, мой друг!'
-        print("{} wrote:".format(self.client_address[0]))
+        print(f"{self.client_address[0]} wrote from {threading.current_thread().name}:")
         print(str(self.data, "utf-8"))
+        input()
         self.data = self.data+s.encode("utf-8")
-        k = k + 1
-        print(k)
-        self.request.sendall(self.data.upper())
+        self.request.sendall(self.data)
 
 
-if __name__ == "__main__":
-    HOST, PORT = socket.gethostname(), 8000
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
+
+def runserver():
+    HOST, PORT = socket.gethostbyname(socket.gethostname()), 8000
+    server = ThreadedTCPServer((HOST, PORT), MyTCPHandler)
     try:
         print(f'Connected to {HOST}:{PORT}')
         print('Quit the server with CTRL-BREAK.')
-        server.serve_forever()
+        server_thread = threading.Thread(target=server.serve_forever)
+        # Exit the server thread when the main thread terminates
+        server_thread.daemon = True
+        server_thread.start()
+        while True:
+            pass
     except KeyboardInterrupt:
         print()
+
+
+if __name__ == "__main__":
+    runserver()
